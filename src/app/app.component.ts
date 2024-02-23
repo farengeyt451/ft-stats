9acc37ad868237e3a1026ed8f2bd0f7d925505dd
Alexander Kislov <strannik11.92@gmail.com>
2024-02-23 13:51:31 +0200

    Add basic sorting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { CommonModule } from '@angular/common';
-import { Component, DoCheck } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { filter, fromEvent, map } from 'rxjs';
-import { FileDropComponent } from './components/file-drop/file-drop.component';
+import { CommonModule, KeyValue } from "@angular/common";
+import { Component, DoCheck } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { filter, fromEvent, map } from "rxjs";
+import { FileDropComponent } from "./components/file-drop/file-drop.component";
 
 interface MatchUp {
   teamName: string;
@@ -12,15 +12,39 @@ interface MatchUp {
   teamStructure: string[];
 }
 
+/**
+ * Ascending (smallest value first)
+ * Descending (largest value first)
+ */
+enum SortingTypesEnum {
+  Default,
+  ASC,
+  DESC,
+}
+
+enum StatKeysEnum {
+  Games = "games",
+  Goals = "goals",
+  Players = "players",
+}
+
+type StatRecord = Record<
+  string,
+  {
+    games: number;
+    goals: number;
+  }
+>;
+
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [CommonModule, RouterOutlet, FileDropComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent implements DoCheck {
-  public readonly title = 'ft-stats';
+  public readonly title = "ft-stats";
 
   public matchUps: MatchUp[] = [];
   public refereeHash: Record<
@@ -30,28 +54,30 @@ export class AppComponent implements DoCheck {
     }
   > = {};
 
-  public statHash: Record<
-    string,
-    Record<
-      string,
-      {
-        games: number;
-        goals: number;
-      }
-    >
-  > = {};
+  public statHash: Record<string, StatRecord> = {};
 
-  public error: string = '';
-  public errorLine: string = '';
+  public statHashMap: {
+    [k: string]: Map<any, any>;
+  } = {};
+
+  public tableSortingState: any = {};
+
+  public error: string = "";
+  public errorLine: string = "";
+
+  public SortingTypesEnum = SortingTypesEnum;
+  public StatKeysEnum = StatKeysEnum;
+  public readonly tableRowHeight = 50;
+  public readonly tableColumnWidth = 220;
 
   constructor() {
-    // this.renderFileContent(this.example);
-    // this.formStats(this.example);
+    this.renderFileContent(this.example);
+    this.formStats(this.example);
   }
 
-  public fileContent: string = '';
+  public fileContent: string = "";
   public example: string = `
-  !ЗАРЯ! – УРАЛМАШ – 3-1 (0-1). 31.03. Ст-н Авангард. 7000 зр. Судья – Лушин. Состав:  Кубышкин, Найденко, Малыгин, Кузовлев, Рабочий, Оленев, Стульчин, Малышенко, Колесников, Куксов, Лукьянчук (Иванов, 46). Голы: Колесников 69, Малышенко 71, Куксов 74.
+  !ЗАРЯ! – УРАЛМАШ – 3-1 (0-1). 31.03. Ст-н Авангард. 7000 зр. Судья – Лушин. Состав:  Кубышкин, Найденко, Малыгин, Кузовлев, Рабочий, Оленев, Стульчин, Малышенко, Колесников, Куксов, Лукьянчук (Иванов, 46). Голы: Колесников, Колесников 69, Малышенко 71, Куксов 74.
 
   !МЕТАЛЛИСТ! – !СКА Од! – 2-1 (1-0). 08.05. Ст-н Металлист. 20000 зр. Судья – Ходеев. Состав: Двуреченский, Дегтярев, Поточняк, Крячко, Ледней, Шаленко (Улинец, 67), Ткаченко (Журавчак, 87), Сааков, Линке (Довбий, 88), Бачиашвили, Шеленков. – Макашвили, Николаенко, Николайшвили (Сафроненко, 46), Клыков, Умрихин, Марусин, Жарков, Смаровоз (Криштан, 46), Беланов, Корюков, Щербина (Попов, 65). Голы: Бачиашвили 15, Бачиашвили 62 – Марусин 66.
   `;
@@ -63,13 +89,13 @@ export class AppComponent implements DoCheck {
     const { files } = file.target as HTMLInputElement;
     files && files.length && reader.readAsText(files[0]);
 
-    fromEvent<ProgressEvent<FileReader>>(reader, 'load')
+    fromEvent<ProgressEvent<FileReader>>(reader, "load")
       .pipe(
         map((val) => val.target?.result),
         filter(Boolean)
       )
       .subscribe((result) => {
-        if (typeof result === 'string') {
+        if (typeof result === "string") {
           this.renderFileContent(result);
           this.formStats(result);
         }
@@ -81,18 +107,28 @@ export class AppComponent implements DoCheck {
   }
 
   formStats(data: string) {
-    const lines = data.split('\n');
+    const lines = data.split("\n");
 
     lines.forEach((line: string) => {
       this.validateLine(line) && this.sliceString(line);
     });
 
     this.createHashTable(this.matchUps);
-    console.log(this.statHash);
+
+    for (let team in this.statHash) {
+      const m = new Map(Object.entries(this.statHash[team]));
+      this.statHashMap[team] = m;
+      this.tableSortingState[team] = {
+        players: SortingTypesEnum.Default,
+        games: SortingTypesEnum.Default,
+        goals: SortingTypesEnum.Default,
+      };
+    }
+    console.log(this.tableSortingState);
   }
 
   validateLine(line: string): boolean {
-    return line.includes('Судья') && line.includes('Состав');
+    return line.includes("Судья") && line.includes("Состав");
   }
 
   renderFileContent(value: string) {
@@ -106,7 +142,7 @@ export class AppComponent implements DoCheck {
 
     if (!res) {
       this.errorLine = `Неправильно отформатирована строка ${line}`;
-      return ' ';
+      return " ";
     }
     return line.substring(MATCHUP_START, MATCHUP_END).trim();
   }
@@ -116,12 +152,12 @@ export class AppComponent implements DoCheck {
       const match = matchUp.match(/!((?:\\.|[^"\\])*)!/g);
       if (!match) {
         this.error = `Неправильные скобки в строке ${matchUp}`;
-        return '';
+        return "";
       } else {
-        return match[0].replace(/!/g, '');
+        return match[0].replace(/!/g, "");
       }
     } else {
-      throw new Error('Quotation marks are not properly placed');
+      throw new Error("Quotation marks are not properly placed");
     }
   }
 
@@ -137,23 +173,23 @@ export class AppComponent implements DoCheck {
   }
 
   getRefereeName(line: string) {
-    const startIndex = line.indexOf('Судья');
-    const endIndex = line.indexOf('Состав');
+    const startIndex = line.indexOf("Судья");
+    const endIndex = line.indexOf("Состав");
 
     if (startIndex !== -1 && endIndex !== -1 && startIndex < endIndex) {
       const referee = line.slice(startIndex + 8, endIndex - 2).trim();
       return referee;
     } else {
       this.error = `Судья не найден, ${line}`;
-      throw new Error('Can not find referee');
+      throw new Error("Can not find referee");
     }
   }
 
   getTeamStructure(line: string) {
-    const startIndex = line.indexOf('Состав');
+    const startIndex = line.indexOf("Состав");
     const lineLength = line.length;
-    const goalIndex = line.indexOf('Гол:');
-    const goalsIndex = line.indexOf('Голы:');
+    const goalIndex = line.indexOf("Гол:");
+    const goalsIndex = line.indexOf("Голы:");
     const gi = goalIndex === -1 ? goalsIndex : goalIndex;
 
     const endIndex = gi === -1 ? lineLength : gi;
@@ -162,37 +198,37 @@ export class AppComponent implements DoCheck {
       return line
         .substring(startIndex + 7, endIndex - 2)
         .trim()
-        .replace(/[()]/g, '')
-        .replace(/\d/g, '')
-        .replace(/;/g, '')
-        .split(',')
-        .map((el) => el.trim().split(' '))
+        .replace(/[()]/g, "")
+        .replace(/\d/g, "")
+        .replace(/;/g, "")
+        .split(",")
+        .map((el) => el.trim().split(" "))
         .flat()
-        .map((el) => el.replace(/\.$/g, ''))
+        .map((el) => el.replace(/\.$/g, ""))
         .filter(Boolean);
     } else {
-      return [''];
+      return [""];
     }
   }
 
   getGoals(line: string) {
-    const startIndex = line.indexOf('Голы:');
-    const endIndex = line.indexOf('Гол:');
+    const startIndex = line.indexOf("Голы:");
+    const endIndex = line.indexOf("Гол:");
     const goalsIndex = startIndex === -1 ? endIndex : startIndex;
 
     if (goalsIndex !== -1) {
       const goalsData = line
         .substring(goalsIndex + 5)
         .trim()
-        .replace(/\d/g, '')
-        .replace(/\.$/g, '')
-        .split(',')
-        .map((el) => el.trim().split(' '))
+        .replace(/\d/g, "")
+        .replace(/\.$/g, "")
+        .split(",")
+        .map((el) => el.trim().split(" "))
         .flat()
         .filter(Boolean);
       return goalsData;
     }
-    return [''];
+    return [""];
   }
 
   sliceString(line: string) {
@@ -213,7 +249,7 @@ export class AppComponent implements DoCheck {
 
     if (this.isDoubleMatchUp(line)) {
       const teamNames = this.extractQuotedTeam(line);
-      const teamDividerIndex = teamStructure.indexOf('–');
+      const teamDividerIndex = teamStructure.indexOf("–");
 
       const teamStructure1 = teamStructure.slice(0, teamDividerIndex);
       const teamStructure2 = teamStructure.slice(
@@ -303,4 +339,49 @@ export class AppComponent implements DoCheck {
       }
     });
   }
+
+  sort(teamName: string, sortKey: StatKeysEnum) {
+    this.sortTableData(teamName, this.statHashMap[teamName], sortKey);
+  }
+
+  // for (let [key, value] of myObject) {
+  //   console.log(key, value);
+  // }
+
+  sortTableData(
+    teamName: string,
+    data: Map<string, { goals: number; games: number }>,
+    sortKey: StatKeysEnum
+  ) {
+    if (sortKey === StatKeysEnum.Goals) {
+      console.log(this.tableSortingState);
+      if (this.tableSortingState[teamName].goals === SortingTypesEnum.Default) {
+        const sortedDesc = new Map(
+          [...data.entries()].sort(([, a], [, b]) => b.goals - a.goals)
+        );
+        this.tableSortingState[teamName].goals = SortingTypesEnum.DESC;
+        this.statHashMap[teamName] = sortedDesc;
+      } else if (
+        this.tableSortingState[teamName].goals === SortingTypesEnum.DESC
+      ) {
+        const sortedAsc = new Map(
+          [...data.entries()].sort(([, a], [, b]) => a.goals - b.goals)
+        );
+        this.tableSortingState[teamName].goals = SortingTypesEnum.ASC;
+        this.statHashMap[teamName] = sortedAsc;
+      } else if (
+        this.tableSortingState[teamName].goals === SortingTypesEnum.ASC
+      ) {
+        const sortedDesc = new Map(
+          [...data.entries()].sort(([, a], [, b]) => b.goals - a.goals)
+        );
+        this.tableSortingState[teamName].goals = SortingTypesEnum.DESC;
+        this.statHashMap[teamName] = sortedDesc;
+      }
+    }
+  }
+
+  originalOrder = (a: KeyValue<any, any>, b: KeyValue<any, any>): number => {
+    return 0;
+  };
 }
